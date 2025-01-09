f518a0fa3105e9908a15e65e7f97acd6295144c7
GediminVV <vitali.gedimin@gmail.com>
2025-01-09 20:27:41 +0300

    Штрафы и дела в json

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -32,40 +32,33 @@ class BotDecorator(Bot):
 
     def _create_work_menu(self):
         keyboard = telebot.types.InlineKeyboardMarkup()
-        for i in range(len(self._work)):
-            keyboard.add(telebot.types.InlineKeyboardButton(text=self._work[i][0],
+        for i, key in enumerate(self._actions["Домашние дела"].keys()):
+            keyboard.add(telebot.types.InlineKeyboardButton(text=key,
                                                             callback_data=f"work_{i}"))
         self._work_keyboard = keyboard
 
     def _create_taxes_menu(self):
         keyboard = telebot.types.InlineKeyboardMarkup()
-        for i in range(len(self._taxes)):
-            keyboard.add(telebot.types.InlineKeyboardButton(text=self._taxes[i][0],
+        for i, key in enumerate(self._actions["Штрафы"].keys()):
+            keyboard.add(telebot.types.InlineKeyboardButton(text=key,
                                                             callback_data=f"taxes_{i}"))
         self._taxes_keyboard = keyboard
 
     def read_settings(self):
-        with open("Settings/settings.json") as settings:
+        with open("Settings/settings.json", encoding='utf-8') as settings:
             data = json.load(settings)
             self._people_id_name = {
                 int(i): name for i, name in data["people_id_name"].items()}
-            self._work = self.read_csv_list(data["files"]["work"])#TODO keyboard creation to method
-            self._taxes = self.read_csv_list(data["files"]["taxes"])
+        with open("Settings/actions.json", encoding='utf-8') as settings:
+            self._actions = json.load(settings)
+            print(self._actions)
             self._create_more_menu()
             self._create_work_menu()
             self._create_taxes_menu()
-        with open("Settings/points.json") as points:
+        with open("Settings/points.json", encoding='utf-8') as points:
             data = json.load(points)
             self._points = {
                 int(i): int(points) for i, points in data.items()}
-
-    def read_csv_list(self, file):
-        with open(file) as f:
-            reader = csv.reader(f)
-            lst = []
-            for i, row in enumerate(reader):
-                lst.append( (row[0], int(row[1])) )  # TODO избавиться от флоатов?
-        return lst
 
     def get_text_messages(self, message):
         if message.from_user.id in self._people_id_name:
@@ -79,8 +72,8 @@ class BotDecorator(Bot):
 
     def log(self, user, list_name, action_id):
         time = datetime.datetime.now()
-        with open("Settings/log.txt", 'a') as log:
-            log.write(f"{time} {self._people_id_name[user]} {getattr(self, '_' + list_name)[action_id][0]} Цена:{getattr(self, '_' + list_name)[action_id][1]} Баланс:{self._points[user]}\n")
+        # with open("Settings/log.txt", 'a') as log:
+            # log.write(f"{time} {self._people_id_name[user]} {getattr(self, '_' + list_name)[action_id][0]} Цена:{getattr(self, '_' + list_name)[action_id][1]} Баланс:{self._points[user]}\n")
 
     def get_balance(self):
         balance = ""
@@ -101,17 +94,19 @@ class BotDecorator(Bot):
                                        text="Выбирай",
                                        reply_markup=getattr(self, "_" + data[1] + "_keyboard"))
             elif data[0] == "work":
+                action = list(self._actions["Домашние дела"].keys())[int(data[1])]
+                action_value = self._actions["Домашние дела"][action]
                 for user in self._people_id_name.keys():
                     if user != call.message.chat.id:
                         keyboard = telebot.types.InlineKeyboardMarkup()
                         keyboard.add(telebot.types.InlineKeyboardButton(text="Отклонить",
                                                                         callback_data=f"decline_{call.message.chat.id}_{data[1]}"))
                         self._bot.send_message(user,
-                                               text=f"{self._people_id_name[call.message.chat.id]} сделал(а) {self._work[int(data[1])][0]}",
+                                               text=f"{self._people_id_name[call.message.chat.id]} сделал(а) {action}",
                                                reply_markup=keyboard)
                     else:
-                        self._points[user] += self._work[int(data[1])][1]
-                        message = f"Умничка, тебе начислено {self._work[int(data[1])][1]} баллов.\nТекущий баланс:{self._points[user]}"
+                        self._points[user] += action_value
+                        message = f"Умничка, тебе начислено {action_value} баллов.\nТекущий баланс:{self._points[user]}"
                         self._bot.send_message( user,
                                                 text=message,
                                                 reply_markup=self._more_keyboard)
@@ -119,8 +114,10 @@ class BotDecorator(Bot):
                         self.save_points()
             elif data[0] == "taxes":
                 user = call.message.chat.id
-                self._points[user] -= self._taxes[int(data[1])][1]
-                message = f"Косяк, косяк... Лови минус {self._taxes[int(data[1])][1]} баллов.\nТекущий баланс:{self._points[user]}"
+                action = list(self._actions["Штрафы"].keys())[int(data[1])]
+                action_value = self._actions["Штрафы"][action]
+                self._points[user] -= action_value
+                message = f"Косяк, косяк... Лови минус {action_value} баллов.\nТекущий баланс:{self._points[user]}"
                 self._bot.send_message(user,
                                        text=message,
                                        reply_markup=self._more_keyboard)
@@ -131,9 +128,11 @@ class BotDecorator(Bot):
                                        text=self.get_balance(),
                                        reply_markup=self._more_keyboard)
             elif data[0] == "decline":
-                self._points[int(data[1])] -= self._work[int(data[2])][1]
+                action = list(self._actions["Домашние дела"].keys())[int(data[2])]
+                action_value = self._actions["Домашние дела"][action]
+                self._points[int(data[1])] -= action_value
                 self._bot.send_message(int(data[1]),
-                                       text=f"{self._people_id_name[call.message.chat.id]} отклонил(а) {self._work[int(data[2])][0]}.\nТекущий баланс:{self._points[int(data[1])]}")
+                                       text=f"{self._people_id_name[call.message.chat.id]} отклонил(а) {action}.\nТекущий баланс:{self._points[int(data[1])]}")
                 self.save_points()
             elif call.data == "to_zero":
                 keyboard = telebot.types.InlineKeyboardMarkup()
